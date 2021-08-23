979c153613f9ed97b112633275e8ee892e0f9c46
EkeMinusYou <euonymuslke+develop@gmail.com>
2021-08-24 00:12:23 +0900

    init firebase-ui

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="signin">
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent} from "vue";
+import firebase from "firebase/app";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+export default defineComponent({
+  name: "Signin",
+  mounted: () => {
+    let ui = firebaseui.auth.AuthUI.getInstance();
+    if (!ui) ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", {
+      signInSuccessUrl: '', // TODO: redirectUrl
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ]
+    });
+  }
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import firebase from 'firebase/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyCdDuOowy22qzYTgwAl_DdKv6pcbHIZAY0",
+  authDomain: "fir-ui-vue-sample.firebaseapp.com",
+};
+firebase.initializeApp(firebaseConfig);
+
 createApp(App).mount('#app')
